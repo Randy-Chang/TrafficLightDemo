c47c6a3114f8f9955ed961923fca4d8eefca0c67
Randy-Chang <gdba5712@gmail.com>
2026-07-18 15:48:33 +0800

    [新增] 建立TrafficLightDemo.FSM專案

--- TrafficLightDemo.FSM/MainForm.Designer.cs
+++ TrafficLightDemo.FSM/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TrafficLightDemo.ManualResetEvent
+﻿namespace TrafficLightDemo.FSM
 {
     partial class MainForm
     {
@@ -29,9 +29,8 @@
         private void InitializeComponent()
         {
             this.plGreenLight = new System.Windows.Forms.Panel();
-            this.plYellowLight = new System.Windows.Forms.Panel();
             this.plRedLight = new System.Windows.Forms.Panel();
-            this.gbTrafficLights = new System.Windows.Forms.GroupBox();
+            this.plYellowLight = new System.Windows.Forms.Panel();
             this.gbDurationSettings = new System.Windows.Forms.GroupBox();
             this.btnApplyDurations = new System.Windows.Forms.Button();
             this.tbRedDuration = new System.Windows.Forms.TextBox();
@@ -40,11 +39,12 @@
             this.lbRedDuration = new System.Windows.Forms.Label();
             this.lbYellowDuration = new System.Windows.Forms.Label();
             this.lbGreenDuration = new System.Windows.Forms.Label();
-            this.lbElapsedTime = new System.Windows.Forms.Label();
-            this.btnStart = new System.Windows.Forms.Button();
+            this.gbTrafficLights = new System.Windows.Forms.GroupBox();
             this.btnPause = new System.Windows.Forms.Button();
-            this.gbTrafficLights.SuspendLayout();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbElapsedTime = new System.Windows.Forms.Label();
             this.gbDurationSettings.SuspendLayout();
+            this.gbTrafficLights.SuspendLayout();
             this.SuspendLayout();
             // 
             // plGreenLight
@@ -55,14 +55,6 @@
             this.plGreenLight.Size = new System.Drawing.Size(100, 100);
             this.plGreenLight.TabIndex = 0;
             // 
-            // plYellowLight
-            // 
-            this.plYellowLight.BackColor = System.Drawing.Color.Black;
-            this.plYellowLight.Location = new System.Drawing.Point(112, 37);
-            this.plYellowLight.Name = "plYellowLight";
-            this.plYellowLight.Size = new System.Drawing.Size(100, 100);
-            this.plYellowLight.TabIndex = 1;
-            // 
             // plRedLight
             // 
             this.plRedLight.BackColor = System.Drawing.Color.Black;
@@ -71,17 +63,13 @@
             this.plRedLight.Size = new System.Drawing.Size(100, 100);
             this.plRedLight.TabIndex = 2;
             // 
-            // gbTrafficLights
+            // plYellowLight
             // 
-            this.gbTrafficLights.Controls.Add(this.plGreenLight);
-            this.gbTrafficLights.Controls.Add(this.plRedLight);
-            this.gbTrafficLights.Controls.Add(this.plYellowLight);
-            this.gbTrafficLights.Location = new System.Drawing.Point(12, 12);
-            this.gbTrafficLights.Name = "gbTrafficLights";
-            this.gbTrafficLights.Size = new System.Drawing.Size(328, 153);
-            this.gbTrafficLights.TabIndex = 3;
-            this.gbTrafficLights.TabStop = false;
-            this.gbTrafficLights.Text = "Traffic Lights";
+            this.plYellowLight.BackColor = System.Drawing.Color.Black;
+            this.plYellowLight.Location = new System.Drawing.Point(112, 37);
+            this.plYellowLight.Name = "plYellowLight";
+            this.plYellowLight.Size = new System.Drawing.Size(100, 100);
+            this.plYellowLight.TabIndex = 1;
             // 
             // gbDurationSettings
             // 
@@ -95,7 +83,7 @@
             this.gbDurationSettings.Location = new System.Drawing.Point(346, 12);
             this.gbDurationSettings.Name = "gbDurationSettings";
             this.gbDurationSettings.Size = new System.Drawing.Size(328, 213);
-            this.gbDurationSettings.TabIndex = 4;
+            this.gbDurationSettings.TabIndex = 14;
             this.gbDurationSettings.TabStop = false;
             this.gbDurationSettings.Text = "Duration Settings (s)";
             // 
@@ -159,50 +147,62 @@
             this.lbGreenDuration.TabIndex = 0;
             this.lbGreenDuration.Text = "Green Light";
             // 
-            // lbElapsedTime
+            // gbTrafficLights
             // 
-            this.lbElapsedTime.AutoSize = true;
-            this.lbElapsedTime.Location = new System.Drawing.Point(13, 181);
-            this.lbElapsedTime.Name = "lbElapsedTime";
-            this.lbElapsedTime.Size = new System.Drawing.Size(112, 29);
-            this.lbElapsedTime.TabIndex = 5;
-            this.lbElapsedTime.Text = "Timer : --";
-            // 
-            // btnStart
-            // 
-            this.btnStart.Location = new System.Drawing.Point(150, 175);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(92, 41);
-            this.btnStart.TabIndex = 6;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.gbTrafficLights.Controls.Add(this.plGreenLight);
+            this.gbTrafficLights.Controls.Add(this.plRedLight);
+            this.gbTrafficLights.Controls.Add(this.plYellowLight);
+            this.gbTrafficLights.Location = new System.Drawing.Point(12, 12);
+            this.gbTrafficLights.Name = "gbTrafficLights";
+            this.gbTrafficLights.Size = new System.Drawing.Size(328, 153);
+            this.gbTrafficLights.TabIndex = 13;
+            this.gbTrafficLights.TabStop = false;
+            this.gbTrafficLights.Text = "Traffic Lights";
             // 
             // btnPause
             // 
             this.btnPause.Location = new System.Drawing.Point(248, 175);
             this.btnPause.Name = "btnPause";
             this.btnPause.Size = new System.Drawing.Size(92, 41);
-            this.btnPause.TabIndex = 12;
+            this.btnPause.TabIndex = 17;
             this.btnPause.Text = "Pause";
             this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(150, 175);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(92, 41);
+            this.btnStart.TabIndex = 16;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // lbElapsedTime
+            // 
+            this.lbElapsedTime.AutoSize = true;
+            this.lbElapsedTime.Location = new System.Drawing.Point(13, 181);
+            this.lbElapsedTime.Name = "lbElapsedTime";
+            this.lbElapsedTime.Size = new System.Drawing.Size(112, 29);
+            this.lbElapsedTime.TabIndex = 15;
+            this.lbElapsedTime.Text = "Timer : --";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(682, 253);
+            this.Controls.Add(this.gbDurationSettings);
+            this.Controls.Add(this.gbTrafficLights);
             this.Controls.Add(this.btnPause);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.lbElapsedTime);
-            this.Controls.Add(this.gbDurationSettings);
-            this.Controls.Add(this.gbTrafficLights);
             this.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "MainForm";
-            this.Text = "Traffic Light State Machine";
-            this.gbTrafficLights.ResumeLayout(false);
+            this.Text = "Form1";
             this.gbDurationSettings.ResumeLayout(false);
             this.gbDurationSettings.PerformLayout();
+            this.gbTrafficLights.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -211,20 +211,20 @@
         #endregion
 
         private System.Windows.Forms.Panel plGreenLight;
-        private System.Windows.Forms.Panel plYellowLight;
         private System.Windows.Forms.Panel plRedLight;
-        private System.Windows.Forms.GroupBox gbTrafficLights;
+        private System.Windows.Forms.Panel plYellowLight;
         private System.Windows.Forms.GroupBox gbDurationSettings;
-        private System.Windows.Forms.Label lbRedDuration;
-        private System.Windows.Forms.Label lbYellowDuration;
-        private System.Windows.Forms.Label lbGreenDuration;
         private System.Windows.Forms.Button btnApplyDurations;
         private System.Windows.Forms.TextBox tbRedDuration;
         private System.Windows.Forms.TextBox tbYellowDuration;
         private System.Windows.Forms.TextBox tbGreenDuration;
-        private System.Windows.Forms.Label lbElapsedTime;
-        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lbRedDuration;
+        private System.Windows.Forms.Label lbYellowDuration;
+        private System.Windows.Forms.Label lbGreenDuration;
+        private System.Windows.Forms.GroupBox gbTrafficLights;
         private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lbElapsedTime;
     }
 }
 
---
+++ TrafficLightDemo.FSM/MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafficLightDemo.FSM
+{
+    public enum MainState
+    {
+        Idle,
+        Active,
+    }
+
+    public enum TrafficLightState
+    {
+        Green,
+        Yellow,
+        Red
+    }
+
+    public partial class MainForm : Form
+    {
+        #region Fields
+
+        private Thread trafficLightWorkerThread;
+        private StateTimer stateTimer;
+
+        private TrafficLightState trafficLightState;
+        private MainState mainState;
+        private int greenDurationSeconds = 5;
+        private int yellowDurationSeconds = 2;
+        private int redDurationSeconds = 5;
+
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeTrafficLight();
+
+            btnApplyDurations.Click += btnApplyDurations_Click;
+            btnStart.Click += btnStart_Click;
+            btnPause.Click += btnPause_Click;
+
+            SetRunButtons(isRunning: false);
+        }
+
+        private void InitializeTrafficLight()
+        {
+            mainState = MainState.Idle;
+            trafficLightState = TrafficLightState.Green;
+            stateTimer = new StateTimer();
+
+            trafficLightWorkerThread = new Thread(RunTrafficLightStateMachine);
+            trafficLightWorkerThread.IsBackground = true;
+            trafficLightWorkerThread.Start();
+        }
+
+        #region UI events
+
+        private void btnApplyDurations_Click(object sender, EventArgs e)
+        {
+            if (!TryReadPositiveSeconds(tbGreenDuration, "綠燈", out int newGreenDurationSeconds) ||
+                !TryReadPositiveSeconds(tbYellowDuration, "黃燈", out int newYellowDurationSeconds) ||
+                !TryReadPositiveSeconds(tbRedDuration, "紅燈", out int newRedDurationSeconds))
+            {
+                return;
+            }
+
+            greenDurationSeconds = newGreenDurationSeconds;
+            yellowDurationSeconds = newYellowDurationSeconds;
+            redDurationSeconds = newRedDurationSeconds;
+            stateTimer.Reset();
+            trafficLightState = TrafficLightState.Green;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            mainState = MainState.Active;
+            stateTimer.Resume();
+            SetRunButtons(isRunning: true);
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            mainState = MainState.Idle;
+            stateTimer.Stop();
+            SetRunButtons(isRunning: false);
+        }
+
+        #endregion
+
+        #region Traffic-light state machine
+
+        private void RunTrafficLightStateMachine()
+        {
+            while (true)
+            {
+                Thread.Sleep(10); // 避免 CPU 過度使用。
+                SetControlText(lbElapsedTime, $"Timer : {stateTimer.ElapsedSeconds}");
+
+                switch (mainState)
+                {
+                    case MainState.Idle:
+                        // 等待使用者按下「開始」按鈕。
+                        break;
+
+                    case MainState.Active:
+                        {
+                            switch (trafficLightState)
+                            {
+                                case TrafficLightState.Green:
+                                    SetControlBackColor(plGreenLight, Color.Green);
+                                    SetControlBackColor(plYellowLight, Color.Black);
+                                    SetControlBackColor(plRedLight, Color.Black);
+
+                                    if (stateTimer.HasElapsed(greenDurationSeconds))
+                                    {
+                                        trafficLightState = TrafficLightState.Yellow;
+                                        stateTimer.Restart();
+                                    }
+                                    break;
+
+                                case TrafficLightState.Yellow:
+                                    SetControlBackColor(plGreenLight, Color.Black);
+                                    SetControlBackColor(plYellowLight, Color.Yellow);
+                                    SetControlBackColor(plRedLight, Color.Black);
+
+                                    if (stateTimer.HasElapsed(yellowDurationSeconds))
+                                    {
+                                        trafficLightState = TrafficLightState.Red;
+                                        stateTimer.Restart();
+                                    }
+                                    break;
+
+                                case TrafficLightState.Red:
+                                    SetControlBackColor(plGreenLight, Color.Black);
+                                    SetControlBackColor(plYellowLight, Color.Black);
+                                    SetControlBackColor(plRedLight, Color.Red);
+
+                                    if (stateTimer.HasElapsed(redDurationSeconds))
+                                    {
+                                        trafficLightState = TrafficLightState.Green;
+                                        stateTimer.Restart();
+                                    }
+                                    break;
+                            }
+                        }
+                        break;
+
+                }
+                
+
+                Thread.Sleep(10);
+            }
+        }
+
+        #endregion
+
+        #region UI helpers
+
+        private bool TryReadPositiveSeconds(TextBox textBox, string lightName, out int seconds)
+        {
+            if (int.TryParse(textBox.Text, out seconds) && seconds > 0)
+            {
+                return true;
+            }
+
+            MessageBox.Show(
+                $"請輸入大於 0 的{lightName}時間（秒）。",
+                "輸入錯誤",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Warning);
+
+            textBox.Focus();
+            textBox.SelectAll();
+            return false;
+        }
+
+        private void SetRunButtons(bool isRunning)
+        {
+            btnStart.Enabled = !isRunning;
+            btnPause.Enabled = isRunning;
+            gbDurationSettings.Enabled = !isRunning;
+        }
+
+        private static void SetControlText(Control control, string text)
+        {
+            if (control.InvokeRequired)
+            {
+                control.BeginInvoke(new Action<Control, string>(SetControlText), control, text);
+            }
+            else
+            {
+                control.Text = text;
+            }
+        }
+
+        private static void SetControlBackColor(Control control, Color color)
+        {
+            if (control.InvokeRequired)
+            {
+                control.BeginInvoke(new Action<Control, Color>(SetControlBackColor), control, color);
+            }
+            else
+            {
+                control.BackColor = color;
+            }
+
+        }
+
+        #endregion
+    }
+
+    public sealed class StateTimer
+    {
+        private readonly Stopwatch stopwatch = new Stopwatch();
+
+        public void Reset()
+        {
+            stopwatch.Reset();
+        }
+
+        public void Resume()
+        {
+            stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            stopwatch.Stop();
+        }
+
+        public void Restart()
+
+        {
+            stopwatch.Restart();
+        }
+
+        public bool HasElapsed(int seconds)
+        {
+            return stopwatch.Elapsed.TotalSeconds >= seconds;
+        }
+
+        public int ElapsedSeconds
+        {
+            get
+            {
+                return (int)stopwatch.Elapsed.TotalSeconds;
+            }
+        }
+    }
+}
